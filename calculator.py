3ebc8b2a8322a3041ce486307430cd7ff8f151c4
한주호 <juho8345@hanmail.net>
2024-09-30 00:05:17 +0900

    add mul div

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def calculator():
+    num1 = float(input("첫 번째 숫자: "))
+    op = input("연산자(+, -, *, /): ")
+    num2 = float(input("두 번째 숫자: "))
+    
+    if op == '+':
+        print(f"결과: {num1 + num2}")
+    elif op == '-':
+        print(f"결과: {num1 - num2}")
+    elif op == '*':
+        print(f"결과: {num1 * num2}")
+    elif op == '/':
+        if num2 != 0:
+            print(f"결과: {num1 / num2}")
+        else:
+            print("0으로 나눌 수 없습니다.")
+    else:
+        print("잘못된 연산자입니다.")
+
+calculator()
